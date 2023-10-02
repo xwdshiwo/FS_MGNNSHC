011a77fe8af09825ae8780a9d53987a587c93973
xwdshiwo <35399345+xwdshiwo@users.noreply.github.com>
2023-10-03 01:38:30 +0800

    Update GraphPurification.py

--- GraphPurification.py
+++ GraphPurification.py
@@ -6,34 +6,42 @@ This program is used to implement graph purification
 import torch
 import networkx as nx
 
-def compute_adjacency_matrix(graph):
-    return torch.tensor(nx.adjacency_matrix(graph).todense(), dtype=torch.float32)
+class GraphProcessor:
+    def __init__(self):
+        pass
 
-def svd_based_filtering(adj_matrix, top_k_ratio):
-    # SVD decomposition
-    U, S, V = torch.svd(adj_matrix)
-    
-    # Determine k based on the top_k_ratio
-    k = int(S.shape[0] * top_k_ratio)
-    
-    # Reconstruct the adjacency matrix using top-k singular values
-    reconstructed = torch.mm(U[:, :k], torch.mm(torch.diag_embed(S[:k]), V[:, :k].t()))
-    
-    return reconstructed
+    def compute_adjacency_matrix(self, graph):
+        return torch.tensor(nx.adjacency_matrix(graph).todense(), dtype=torch.float32)
 
-def filter_graphs(graphs, top_k_ratio=0.9):
-    purified_adj_matrices = []
-    for graph in graphs:
-        adj_matrix = compute_adjacency_matrix(graph)
-        purified_matrix = svd_based_filtering(adj_matrix, top_k_ratio)
-        purified_adj_matrices.append(purified_matrix)
-    return purified_adj_matrices
+    def svd_based_filtering(self, adj_matrix, top_k_ratio=0.9):
+        # SVD decomposition
+        U, S, V = torch.svd(adj_matrix)
 
-# Example usage:
-# Define a list of graphs using NetworkX
-graphs = [nx.gnp_random_graph(10, 0.5), nx.gnp_random_graph(10, 0.4)]
-purified_adj_matrices = filter_graphs(graphs)
+        # Determine k based on the top_k_ratio
+        k = int(S.shape[0] * top_k_ratio)
 
-for matrix in purified_adj_matrices:
-    print(matrix)
+        # Reconstruct the adjacency matrix using top-k singular values
+        reconstructed = torch.mm(U[:, :k], torch.mm(torch.diag_embed(S[:k]), V[:, :k].t()))
 
+        return reconstructed
+
+    def filter_graph_adj_matrices(self, adj_matrices, top_k_ratio=0.9):
+        purified_adj_matrices = []
+        for adj_matrix in adj_matrices:
+            purified_matrix = self.svd_based_filtering(adj_matrix, top_k_ratio)
+            purified_adj_matrices.append(purified_matrix)
+        return purified_adj_matrices
+
+# # Example usage:
+
+# # Define a list of adjacency matrices for your graphs
+# adj_matrices = [
+#     GraphProcessor().compute_adjacency_matrix(nx.gnp_random_graph(10, 0.5)),
+#     GraphProcessor().compute_adjacency_matrix(nx.gnp_random_graph(10, 0.4))
+# ]
+
+# processor = GraphProcessor()
+# purified_adj_matrices = processor.filter_graph_adj_matrices(adj_matrices)
+
+# for matrix in purified_adj_matrices:
+#     print(matrix)
