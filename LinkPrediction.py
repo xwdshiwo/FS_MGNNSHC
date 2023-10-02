bd99a22a5b4212f6747223dc2510f0c6d30e3476
xwdshiwo <35399345+xwdshiwo@users.noreply.github.com>
2023-10-03 01:38:46 +0800

    Update LinkPrediction.py

--- LinkPrediction.py
+++ LinkPrediction.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 """
 This program is used to implement link prediction
 """
@@ -6,7 +7,7 @@ This program is used to implement link prediction
 import torch
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
-from torch_geometric.utils import negative_sampling, erdos_renyi_graph
+from torch_geometric.utils import negative_sampling, dense_to_sparse
 from torch_geometric.data import Data
 
 class GCN(torch.nn.Module):
@@ -23,63 +24,69 @@ class GCN(torch.nn.Module):
         x = F.dropout(x, p=0.2, training=self.training)
         return self.conv3(x, edge_index)
 
-# Training procedure
-def train():
-    model.train()
+class LinkPrediction:
+    def __init__(self, adj_matrix, node_features, hidden_channels=16, lr=0.01, weight_decay=0.001):
+        self.data = Data(x=node_features, edge_index=dense_to_sparse(adj_matrix)[0])
+        self.data.train_pos_edge_index = self.data.edge_index
+        self.data.test_pos_edge_index = self.data.edge_index
 
-    optimizer.zero_grad()
-    z = model(data.x, data.train_pos_edge_index)
-    
-    # Compute loss based on positive and negative edges
-    pos_edge_logits = torch.sigmoid((z[data.train_pos_edge_index[0]] * z[data.train_pos_edge_index[1]]).sum(dim=1))
-    neg_edge_index = negative_sampling(edge_index=data.train_pos_edge_index, num_nodes=data.num_nodes, num_neg_samples=data.train_pos_edge_index.size(1))
-    neg_edge_logits = torch.sigmoid((z[neg_edge_index[0]] * z[neg_edge_index[1]]).sum(dim=1))
-    
-    loss = F.binary_cross_entropy_with_logits(torch.cat([pos_edge_logits, neg_edge_logits]), torch.cat([torch.ones(pos_edge_logits.size(0)), torch.zeros(neg_edge_logits.size(0))]))
-    loss.backward()
-    optimizer.step()
+        self.model = GCN(num_features=node_features.shape[1], hidden_channels=hidden_channels)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr, weight_decay=weight_decay)
 
-    return loss.item()
+    def train(self):
+        self.model.train()
 
-# Testing procedure
-def test(edge_index, eval_mode):
-    model.eval()
+        self.optimizer.zero_grad()
+        z = self.model(self.data.x, self.data.train_pos_edge_index)
 
-    with torch.no_grad():
-        z = model(data.x, edge_index)
+        pos_edge_logits = torch.sigmoid((z[self.data.train_pos_edge_index[0]] * z[self.data.train_pos_edge_index[1]]).sum(dim=1))
+        neg_edge_index = negative_sampling(edge_index=self.data.train_pos_edge_index, num_nodes=self.data.num_nodes, num_neg_samples=self.data.train_pos_edge_index.size(1))
+        neg_edge_logits = torch.sigmoid((z[neg_edge_index[0]] * z[neg_edge_index[1]]).sum(dim=1))
 
-    # Evaluate MRR on positive and negative edges
-    pos_edge_logits = (z[edge_index[0]] * z[edge_index[1]]).sum(dim=1)
-    neg_edge_index = negative_sampling(edge_index=edge_index, num_nodes=data.num_nodes, num_neg_samples=edge_index.size(1))
-    neg_edge_logits = (z[neg_edge_index[0]] * z[neg_edge_index[1]]).sum(dim=1)
-    
-    pos_ranks = torch.argsort(torch.cat([pos_edge_logits, neg_edge_logits])).argsort()[:edge_index.size(1)]
-    ranks = pos_ranks + 1
-    mrr = (1 / ranks).mean().item()
+        loss = F.binary_cross_entropy_with_logits(torch.cat([pos_edge_logits, neg_edge_logits]), torch.cat([torch.ones(pos_edge_logits.size(0)), torch.zeros(neg_edge_logits.size(0))]))
+        loss.backward(retain_graph=True)
+        self.optimizer.step()
 
-    return mrr
+        return loss.item()
 
-# Randomly generate a graph using Erdos-Renyi model
-num_nodes = 100
-edge_index = erdos_renyi_graph(num_nodes=num_nodes, edge_prob=0.05)
-x = torch.randn((num_nodes, 16))  # Random node features
-data = Data(x=x, edge_index=edge_index)
+    def test(self):
+        self.model.eval()
 
-# Split edges for training and testing (for simplicity, we use the same set here)
-data.train_pos_edge_index = edge_index
-data.test_pos_edge_index = edge_index
+        with torch.no_grad():
+            z = self.model(self.data.x, self.data.test_pos_edge_index)
 
-# Define a model and optimizer
-model = GCN(num_features=16, hidden_channels=16)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=0.001)
+        pos_edge_logits = (z[self.data.test_pos_edge_index[0]] * z[self.data.test_pos_edge_index[1]]).sum(dim=1)
+        neg_edge_index = negative_sampling(edge_index=self.data.test_pos_edge_index, num_nodes=self.data.num_nodes, num_neg_samples=self.data.test_pos_edge_index.size(1))
+        neg_edge_logits = (z[neg_edge_index[0]] * z[neg_edge_index[1]]).sum(dim=1)
 
-best_mrr = 0
-for epoch in range(1, 101):
-    loss = train()
-    mrr = test(data.test_pos_edge_index, eval_mode=True)
-    if mrr > best_mrr:
-        best_mrr = mrr
-        best_data = Data(x=data.x, edge_index=data.edge_index)
-    print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, MRR: {mrr:.4f}')
+        pos_ranks = torch.argsort(torch.cat([pos_edge_logits, neg_edge_logits])).argsort()[:self.data.test_pos_edge_index.size(1)]
+        ranks = pos_ranks + 1
+        mrr = (1 / ranks).mean().item()
 
-print(f'Best MRR: {best_mrr:.4f}')
+        return mrr
+
+    def predict(self):
+        self.model.eval()
+        with torch.no_grad():
+            z = self.model(self.data.x, self.data.edge_index)
+        edge_scores = (z[self.data.edge_index[0]] * z[self.data.edge_index[1]]).sum(dim=1)
+        return edge_scores
+
+# Example usage:
+
+# num_nodes = 100
+# adj_matrix = torch.bernoulli(0.05 * torch.ones((num_nodes, num_nodes)))
+# node_features = torch.randn((num_nodes, 16))
+
+# lp = LinkPrediction(adj_matrix, node_features)
+
+# best_mrr = 0
+# for epoch in range(1, 101):
+#     loss = lp.train()
+#     mrr = lp.test()
+#     if mrr > best_mrr:
+#         best_mrr = mrr
+#     print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, MRR: {mrr:.4f}')
+
+# predicted_adj_matrix = lp.predict()
+# print(predicted_adj_matrix)
